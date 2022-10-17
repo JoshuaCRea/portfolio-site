8cfffd2f6eed126001705114d2f47f19fbb1a271
Joshua Rea <josh.rea@gmail.com>
2022-10-17 14:09:56 -0400

    Refatored 'didPlayerHaveBetterHand'

--- three-card-poker/three-card-poker.js
+++ three-card-poker/three-card-poker.js
@@ -434,25 +434,23 @@ function _determineHandType(hand) {
 };
 
 function _didPlayerHaveBetterHand(pHand, dHand) {
-    if (_isTheHandAStraightFlush(pHand) && !_isTheHandAStraightFlush(dHand)) {
+    const hierarchy = {
+        "straightFlush": 5,
+        "threeOfAKind": 4,
+        "straight": 3,
+        "flush": 2,
+        "pair": 1,
+        undefined: 0,
+    }
+    let playerHandRank = hierarchy[_determineHandType(pHand)];
+    let dealerHandRank = hierarchy[_determineHandType(dHand)];
+    if (playerHandRank > dealerHandRank) {
         return true;
     }
-    if (!_isTheHandAStraightFlush(pHand) && _isTheHandAStraightFlush(dHand)) {
+    if (dealerHandRank > playerHandRank) {
         return false;
     }
-    if (_isTheHandAThreeOfAKind(pHand) && !_isTheHandAThreeOfAKind(dHand)) {
-        return true;
-    }
-    if (!_isTheHandAThreeOfAKind(pHand) && _isTheHandAThreeOfAKind(dHand)) {
-        return false;
-    }
-    if (_isTheHandAStraight(pHand) && !_isTheHandAStraight(dHand)) {
-        return true;
-    }
-    if (!_isTheHandAStraight(pHand) && _isTheHandAStraight(dHand)) {
-        return false;
-    }
-    if (_isTheHandAStraight(pHand) && _isTheHandAStraight(dHand)) {
+    if (_isTheHandAStraight(pHand) || _isTheHandAStraightFlush(pHand)) {
         if (!_isTheHandAWheelStraight(pHand) && _isTheHandAWheelStraight(dHand)) {
             return true;
         }
@@ -460,22 +458,7 @@ function _didPlayerHaveBetterHand(pHand, dHand) {
             return false;
         }
     }
-    if (_isTheHandAFlush(pHand) && !_isTheHandAFlush(dHand)) {
-        return true;
-    }
-    if (!_isTheHandAFlush(pHand) && _isTheHandAFlush(dHand)) {
-        return false;
-    }
-    if (_isTheHandAFlush(pHand) && _isTheHandAFlush(dHand)) {
-        return _didPlayerWinHighCardTieBreaker(pHand, dHand);
-    }
-    if (_isTheHandAPair(pHand) && !_isTheHandAPair(dHand)) {
-        return true;
-    }
-    if (!_isTheHandAPair(pHand) && _isTheHandAPair(dHand)) {
-        return false;
-    }
-    if (_isTheHandAPair(pHand) && _isTheHandAPair(dHand)) {
+    if (_isTheHandAPair(pHand)) {
         const playerPair = CARD_RANKS[Object.entries(new Counter(pHand.map(card => card.charAt(0)))).filter(rank => rank[1] === 2).toString(10)[0]];
         const dealerPair = CARD_RANKS[Object.entries(new Counter(dHand.map(card => card.charAt(0)))).filter(rank => rank[1] === 2).toString(10)[0]];
         if (playerPair > dealerPair) {
@@ -718,35 +701,35 @@ function _getShuffledDeck() {
     return newDeck;
 }
 
-window.onload = () => {
-    const CLICK_BEHAVIORS = {
-        "#ante-diamond": "ANTE",
-        "#pair-plus-wager-circle": "PAIR_PLUS",
-        "#six-card-wager-circle": "SIX_CARD_BONUS",
-    };
-    Object.keys(CLICK_BEHAVIORS).forEach(elementId => {
-        $(elementId).click(function () {
-            if (isRoundActive) {
-                return;
-            }
-            placeWager(_getSelectedWagerAmount(), CLICK_BEHAVIORS[elementId]);
-        });
-    });
-    $("#player-balance-display").html(`$${playerBalance}`);
-    $("#total-wager-display").html(`$${totalWagerAmount}`);
-    $("#deal-button").click(() => dealToPlayer());
-    $("#play-button").click(() => playGame());
-    $("#rebet-button").click(() => rebet());
-    $("#fold-button").click(() => fold());
-    $("#anteWinnings").html("$0");
-    $("#playWinnings").html("$0");
-    $("#anteBonusWinnings").html("$0");
-    $("#pairPlusBonusWinnings").html("$0");
-    $("#sixCardBonusWinnings").html("$0");
-    $("#totalWinnings").html("$0");
-    const infoBoxMessage = 'Place your bets, then click "Deal."';
-    $("#infoBox").html(infoBoxMessage);
-}
+// window.onload = () => {
+//     const CLICK_BEHAVIORS = {
+//         "#ante-diamond": "ANTE",
+//         "#pair-plus-wager-circle": "PAIR_PLUS",
+//         "#six-card-wager-circle": "SIX_CARD_BONUS",
+//     };
+//     Object.keys(CLICK_BEHAVIORS).forEach(elementId => {
+//         $(elementId).click(function () {
+//             if (isRoundActive) {
+//                 return;
+//             }
+//             placeWager(_getSelectedWagerAmount(), CLICK_BEHAVIORS[elementId]);
+//         });
+//     });
+//     $("#player-balance-display").html(`$${playerBalance}`);
+//     $("#total-wager-display").html(`$${totalWagerAmount}`);
+//     $("#deal-button").click(() => dealToPlayer());
+//     $("#play-button").click(() => playGame());
+//     $("#rebet-button").click(() => rebet());
+//     $("#fold-button").click(() => fold());
+//     $("#anteWinnings").html("$0");
+//     $("#playWinnings").html("$0");
+//     $("#anteBonusWinnings").html("$0");
+//     $("#pairPlusBonusWinnings").html("$0");
+//     $("#sixCardBonusWinnings").html("$0");
+//     $("#totalWinnings").html("$0");
+//     const infoBoxMessage = 'Place your bets, then click "Deal."';
+//     $("#infoBox").html(infoBoxMessage);
+// }
 
 // TESTS
 
@@ -756,41 +739,41 @@ window.onload = () => {
 // console.log(_determineHandType(["6D", "JD", "AD"]) === "flush")
 // console.log(_determineHandType(["6D", "JC", "6H"]) === "pair")
 // console.log(_determineHandType(["6D", "JD", "3H"]) === undefined)
-// console.log(_didPlayerHaveBetterHand(["TD", "JD", "9D"], ["KC", "KH", "KS"]) === true); // player has mid straight flush, dealer has trips
-// console.log(_didPlayerHaveBetterHand(["KC", "KH", "KS"], ["TD", "JD", "9D"]) === false); // dealer has mid straight flush, player has trips
-// console.log(_didPlayerHaveBetterHand(["3D", "4H", "5S"], ["7C", "8C", "9C"]) === false); // dealer has mid straight flush, player has straight
-// console.log(_didPlayerHaveBetterHand(["3D", "2D", "AD"], ["KC", "6C", "9C"]) === true); // player has wheel straight flush, dealer has flush
-// console.log(_didPlayerHaveBetterHand(["3S", "2S", "AS"], ["8H", "7H", "9H"]) === false); // player has wheel straight flush, dealer has mid straight flush
-// console.log(_didPlayerHaveBetterHand(["8H", "TH", "9H"], ["3C", "2C", "AC"]) === true); // player has mid straight flush, dealer has wheel straight flush
-// console.log(_didPlayerHaveBetterHand(["8H", "TH", "9H"], ["8C", "9C", "TC"]) === false); // straight flushes tied
-// console.log(_didPlayerHaveBetterHand(["TD", "JS", "9C"], ["6C", "6H", "6S"]) === false); // player has straight, dealer has trips
-// console.log(_didPlayerHaveBetterHand(["4D", "4S", "4C"], ["6C", "7H", "8S"]) === true); // dealer has straight, player has trips
-// console.log(_didPlayerHaveBetterHand(["8D", "8S", "8C"], ["7C", "7H", "7S"]) === true); // both have trips, player's is higher
-// console.log(_didPlayerHaveBetterHand(["JD", "JS", "JC"], ["QC", "QH", "QS"]) === false); // both have trips, dealer's is higher
-// console.log(_didPlayerHaveBetterHand(["TD", "JS", "9C"], ["KC", "6H", "9S"]) === true); // player has mid straight, dealer does not
-// console.log(_didPlayerHaveBetterHand(["3D", "AS", "9C"], ["7C", "8H", "9S"]) === false); // dealer has mid straight, player does not
-// console.log(_didPlayerHaveBetterHand(["3D", "2S", "AC"], ["KC", "6H", "9S"]) === true); // player has wheel straight, dealer has none
-// console.log(_didPlayerHaveBetterHand(["3D", "2S", "AC"], ["8C", "7H", "9S"]) === false); // player has wheel straight, dealer has mid straight
-// console.log(_didPlayerHaveBetterHand(["8C", "TH", "9S"], ["3D", "2S", "AC"]) === true); // player has mid straight, dealer has wheel straight
-// console.log(_didPlayerHaveBetterHand(["4D", "JS", "9C"], ["3C", "AD", "2S"]) === false); // dealer has wheel straight, player has none
-// console.log(_didPlayerHaveBetterHand(["AD", "KS", "QC"], ["8C", "TH", "9S"]) === true); // both have straights, player's is higher
-// console.log(_didPlayerHaveBetterHand(["TD", "JS", "9C"], ["KC", "JH", "QS"]) === false); // both have straights, dealer's is higher
-// console.log(_didPlayerHaveBetterHand(["3C", "7C", "9C"], ["4C", "TH", "2S"]) === true); // player has flush, dealer does not
-// console.log(_didPlayerHaveBetterHand(["7C", "7S", "9D"], ["4H", "TH", "2H"]) === false); // dealer has flush, player does not
-// console.log(_didPlayerHaveBetterHand(["JC", "7C", "9C"], ["4H", "TH", "2H"]) === true); // player has higher flush than dealer
-// console.log(_didPlayerHaveBetterHand(["3C", "7C", "9C"], ["4H", "TH", "2H"]) === false); // dealer has higher flush than player
-// console.log(_didPlayerHaveBetterHand(["3C", "7C", "9C"], ["3H", "7H", "9H"]) === false); // both have tied flush, player has better high card
-// console.log(_didPlayerHaveBetterHand(["3C", "7H", "9S"], ["4C", "TH", "2S"]) === false); //dealer has highest card
-// console.log(_didPlayerHaveBetterHand(["3C", "QH", "9S"], ["4C", "TH", "2S"]) === true); //player has highest card
-// console.log(_didPlayerHaveBetterHand(["TC", "4H", "2D"], ["4C", "TH", "2S"]) === false); //both have equal cards
-// console.log(_didPlayerHaveBetterHand(["3C", "7H", "9S"], ["4C", "8H", "8S"]) === false); // dealer has pair, player has highest card
-// console.log(_didPlayerHaveBetterHand(["3C", "3H", "9S"], ["4C", "TH", "2S"]) === true); // player has pair, dealer has highest card
-// console.log(_didPlayerHaveBetterHand(["2C", "4H", "4D"], ["4C", "4S", "7S"]) === false); // both have equal pairs, dealer has high card outside of pair
-// console.log(_didPlayerHaveBetterHand(["7C", "4H", "4D"], ["4C", "4S", "2S"]) === true); // both have equal pairs, player has high card outside of pair
-// console.log(_didPlayerHaveBetterHand(["TC", "4H", "4D"], ["4C", "4S", "TS"]) === false); // both have equal pairs, equal high cards
-// console.log(_didPlayerHaveBetterHand(["7C", "3H", "3D"], ["8C", "2D", "2S"]) === true); // both have pairs, player's pair is higher
-// console.log(_didPlayerHaveBetterHand(["8C", "2D", "2S"], ["7C", "3H", "3D"]) === false); // both have pairs, dealer's pair is higher
-// console.log(_didPlayerHaveBetterHand(["8C", "2D", "2S"], ["7C", "3H", "6D"]) === true); // player beats dealer, dealer doesn't qualify
+console.log(_didPlayerHaveBetterHand(["TD", "JD", "9D"], ["KC", "KH", "KS"]) === true); // player has mid straight flush, dealer has trips
+console.log(_didPlayerHaveBetterHand(["KC", "KH", "KS"], ["TD", "JD", "9D"]) === false); // dealer has mid straight flush, player has trips
+console.log(_didPlayerHaveBetterHand(["3D", "4H", "5S"], ["7C", "8C", "9C"]) === false); // dealer has mid straight flush, player has straight
+console.log(_didPlayerHaveBetterHand(["3D", "2D", "AD"], ["KC", "6C", "9C"]) === true); // player has wheel straight flush, dealer has flush
+console.log(_didPlayerHaveBetterHand(["3S", "2S", "AS"], ["8H", "7H", "9H"]) === false); // player has wheel straight flush, dealer has mid straight flush
+console.log(_didPlayerHaveBetterHand(["8H", "TH", "9H"], ["3C", "2C", "AC"]) === true); // player has mid straight flush, dealer has wheel straight flush
+console.log(_didPlayerHaveBetterHand(["8H", "TH", "9H"], ["8C", "9C", "TC"]) === false); // straight flushes tied
+console.log(_didPlayerHaveBetterHand(["TD", "JS", "9C"], ["6C", "6H", "6S"]) === false); // player has straight, dealer has trips
+console.log(_didPlayerHaveBetterHand(["4D", "4S", "4C"], ["6C", "7H", "8S"]) === true); // dealer has straight, player has trips
+console.log(_didPlayerHaveBetterHand(["8D", "8S", "8C"], ["7C", "7H", "7S"]) === true); // both have trips, player's is higher
+console.log(_didPlayerHaveBetterHand(["JD", "JS", "JC"], ["QC", "QH", "QS"]) === false); // both have trips, dealer's is higher
+console.log(_didPlayerHaveBetterHand(["TD", "JS", "9C"], ["KC", "6H", "9S"]) === true); // player has mid straight, dealer does not
+console.log(_didPlayerHaveBetterHand(["3D", "AS", "9C"], ["7C", "8H", "9S"]) === false); // dealer has mid straight, player does not
+console.log(_didPlayerHaveBetterHand(["3D", "2S", "AC"], ["KC", "6H", "9S"]) === true); // player has wheel straight, dealer has none
+console.log(_didPlayerHaveBetterHand(["3D", "2S", "AC"], ["8C", "7H", "9S"]) === false); // player has wheel straight, dealer has mid straight
+console.log(_didPlayerHaveBetterHand(["8C", "TH", "9S"], ["3D", "2S", "AC"]) === true); // player has mid straight, dealer has wheel straight
+console.log(_didPlayerHaveBetterHand(["4D", "JS", "9C"], ["3C", "AD", "2S"]) === false); // dealer has wheel straight, player has none
+console.log(_didPlayerHaveBetterHand(["AD", "KS", "QC"], ["8C", "TH", "9S"]) === true); // both have straights, player's is higher
+console.log(_didPlayerHaveBetterHand(["TD", "JS", "9C"], ["KC", "JH", "QS"]) === false); // both have straights, dealer's is higher
+console.log(_didPlayerHaveBetterHand(["3C", "7C", "9C"], ["4C", "TH", "2S"]) === true); // player has flush, dealer does not
+console.log(_didPlayerHaveBetterHand(["7C", "7S", "9D"], ["4H", "TH", "2H"]) === false); // dealer has flush, player does not
+console.log(_didPlayerHaveBetterHand(["JC", "7C", "9C"], ["4H", "TH", "2H"]) === true); // player has higher flush than dealer
+console.log(_didPlayerHaveBetterHand(["3C", "7C", "9C"], ["4H", "TH", "2H"]) === false); // dealer has higher flush than player
+console.log(_didPlayerHaveBetterHand(["3C", "7C", "9C"], ["3H", "7H", "9H"]) === false); // both have tied flush, player has better high card
+console.log(_didPlayerHaveBetterHand(["3C", "7H", "9S"], ["4C", "TH", "2S"]) === false); //dealer has highest card
+console.log(_didPlayerHaveBetterHand(["3C", "QH", "9S"], ["4C", "TH", "2S"]) === true); //player has highest card
+console.log(_didPlayerHaveBetterHand(["TC", "4H", "2D"], ["4C", "TH", "2S"]) === false); //both have equal cards
+console.log(_didPlayerHaveBetterHand(["3C", "7H", "9S"], ["4C", "8H", "8S"]) === false); // dealer has pair, player has highest card
+console.log(_didPlayerHaveBetterHand(["3C", "3H", "9S"], ["4C", "TH", "2S"]) === true); // player has pair, dealer has highest card
+console.log(_didPlayerHaveBetterHand(["2C", "4H", "4D"], ["4C", "4S", "7S"]) === false); // both have equal pairs, dealer has high card outside of pair
+console.log(_didPlayerHaveBetterHand(["7C", "4H", "4D"], ["4C", "4S", "2S"]) === true); // both have equal pairs, player has high card outside of pair
+console.log(_didPlayerHaveBetterHand(["TC", "4H", "4D"], ["4C", "4S", "TS"]) === false); // both have equal pairs, equal high cards
+console.log(_didPlayerHaveBetterHand(["7C", "3H", "3D"], ["8C", "2D", "2S"]) === true); // both have pairs, player's pair is higher
+console.log(_didPlayerHaveBetterHand(["8C", "2D", "2S"], ["7C", "3H", "3D"]) === false); // both have pairs, dealer's pair is higher
+console.log(_didPlayerHaveBetterHand(["8C", "2D", "2S"], ["7C", "3H", "6D"]) === true); // player beats dealer, dealer doesn't qualify
 // console.log(_isTheHandAFiveCardStraight(["2C", "3D", "7S", "8C", "9H", "TD"]) === false);
 // console.log(_isTheHandAFiveCardStraight(["8C", "5D", "3S", "7C", "6H", "4D"]) === true); // straight, 3 - 8
 // console.log(_isTheHandAFiveCardStraight(["8C", "5D", "2S", "7C", "6H", "4D"]) === true); // straight, 4 - 8, sixth card below
