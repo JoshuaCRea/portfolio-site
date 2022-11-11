6f2c5fca362c770b8cc5f264fc954a372633e877
Joshua Rea <josh.rea@gmail.com>
2022-11-10 22:35:41 -0500

    Created 'Current Turn' infoBox and hooked up all its data.

--- votsw/app.js
+++ votsw/app.js
@@ -1,7 +1,8 @@
 const LOCATION_IDS = ["#Leap-Creek", "#roadLCBS", "#Blackstone", "#roadBSFM", "#Fangmarsh", "#roadFMUC", "#Underclaw", "#roadUCP", "#Pouch", "#roadPLC"];
 const UNOCCUPIED_LOCATION_COLOR = 'rgb(165, 143, 106)';
-const CW_DIR_VALUE = 1
-const CCW_DIR_VALUE = -1
+const CW_DIR_VALUE = 1;
+const CCW_DIR_VALUE = -1;
+
 const TOWN_DESCRIPTIONS = {
     '#Leap-Creek': {
         nickname: "The Water Temple",
@@ -20,54 +21,53 @@ const TOWN_DESCRIPTIONS = {
         schoolName: "Kwoon of Changquan",
     },
     '#Pouch': {
-        nickname: "Forest of Wine and Shadow",
+        nickname: "The Shadow Forest",
         schoolName: "School of Zui Quan",
     }
 }
 // Create an InfoBox
 // Create turn tracker
 
-
 const playerInfo = {
     p1: {
         color: '#47c3ed',
         startingLocationIndex: 0,
         townCode: "lc",
+        homeTown: "Leap-Creek",
         townInfoId: "#p1TownInfo",
         townSchoolId: "#p1TownSchool",
-        "injured-pip": "https://ucarecdn.com/56cfe13f-8af4-40f5-9c8c-d6cc391ddeab/LC_I.png",
     },
     p2: {
         color: "gray",
         startingLocationIndex: 2,
         townCode: "bs",
+        homeTown: "Blackstone",
         townInfoId: "#p2TownInfo",
         townSchoolId: "#p2TownSchool",
-        "injured-pip": "https://ucarecdn.com/2c4827a8-7afd-4097-abe7-9ba774939f7c/",
     },
     p3: {
         color: "crimson",
         startingLocationIndex: 4,
         townCode: "fm",
+        homeTown: "Fangmarsh",
         townInfoId: "#p3TownInfo",
         townSchoolId: "#p3TownSchool",
-        "injured-pip": "https://ucarecdn.com/7af7e998-bc7c-4bdd-841c-ea3ee5ca424d/FM_I.png",
     },
     p4: {
         color: "green",
         startingLocationIndex: 6,
         townCode: "uc",
+        homeTown: "Underclaw",
         townInfoId: "#p4TownInfo",
         townSchoolId: "#p4TownSchool",
-        "injured-pip": "https://ucarecdn.com/48fb1241-13d4-49cf-b4c8-78102c143cb0/UC_I.png",
     },
     p5: {
         color: "blueviolet",
         startingLocationIndex: 8,
         townCode: "px",
+        homeTown: "Pouch",
         townInfoId: "#p5TownInfo",
         townSchoolId: "#p5TownSchool",
-        "injured-pip": "https://ucarecdn.com/e1dd035b-9c7b-4e7b-a3bb-4801076a82e6/PX_I.png",
     },
 }
 
@@ -76,9 +76,18 @@ class PC {
         this.locationIndex = playerInfo[playerNumber].startingLocationIndex;
         this.townCode = playerInfo[playerNumber].townCode;
         this.injured = false;
+        this.actions = 2;
+        this.repRank = 3;
+        this.pow = 0;
+        this.sta = 0;
+        this.agi = 0;
+        this.chi = 0;
+        this.wit = 0;
+        this.techniques = [];
     };
     _loseFight() {
         this.injured = true;
+        this.repRank -= 1;
     }
     _heal() {
         this.injured = false;
@@ -92,6 +101,49 @@ const playerList = [
     new PC('p4'),
     new PC('p5')
 ];
+
+playerList[0].chi += 2;
+playerList[0].agi += 1;
+playerList[1].sta += 2;
+playerList[1].chi += 1;
+playerList[2].pow += 2;
+playerList[2].wit += 1;
+playerList[3].agi += 2;
+playerList[3].pow += 1;
+playerList[4].wit += 2;
+playerList[4].sta += 1;
+
+let currentPlayerIndex = 0;
+
+function _updateInfoBox() {
+    let currentPlayer = Object.keys(playerInfo)[currentPlayerIndex];
+    $("#playerName").html((`${currentPlayer.toUpperCase()}`));
+    $("#playerTitle").html((`The Light of ${playerInfo[currentPlayer].homeTown}`));
+    $("#playerName").css("background-color", `${playerInfo[currentPlayer].color}`);
+    $("#playerTitle").css("background-color", `${playerInfo[currentPlayer].color}`);
+    $("#powerRank").html((`${playerList[currentPlayerIndex].pow}`));
+    $("#staminaRank").html((`${playerList[currentPlayerIndex].sta}`));
+    $("#agilityRank").html((`${playerList[currentPlayerIndex].agi}`));
+    $("#chiRank").html((`${playerList[currentPlayerIndex].chi}`));
+    $("#witRank").html((`${playerList[currentPlayerIndex].wit}`));
+    $("#townName").html((`${playerInfo[currentPlayer].homeTown}`));
+    $("#nextPlayer").html((`${Object.keys(playerInfo)[currentPlayerIndex + 1]}`));
+    $("#techAmount").html((`${playerList[currentPlayerIndex].techniques.length}`));
+    if (playerList[currentPlayerIndex].repRank <= 2) {
+        $("#repRank").css("background-color", "#000000");
+    }
+    if (playerList[currentPlayerIndex].repRank === 5) {
+        $("#repRank").css("background-color", "#e2b155");
+    } else { $("#repRank").css("background-color", "#a58f6a"); }
+    if (playerList[currentPlayerIndex].injured === false) {
+        $("#healthy-injured").html("Healthy");
+    }
+    if (playerList[currentPlayerIndex].injured === true) {
+        $("#healthy-injured").html("Injured");
+    }
+}
+
+_updateInfoBox();
 
 function updateLocationIndex(directionValue, player) {
     player.locationIndex = (((player.locationIndex + directionValue) % LOCATION_IDS.length) + LOCATION_IDS.length) % LOCATION_IDS.length;
